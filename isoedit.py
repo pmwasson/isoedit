1188e7e11420fdf12d3e6d3ca436e178188fed80
pmwasson <pmwasson@gmail.com>
2021-02-27 13:06:23 -0800

    Refactor canvas

--- isoedit.py
+++ isoedit.py
@@ -23,33 +23,59 @@ PREVIEWSCALE = 2
 PREVIEWHEIGHT = HEIGHT*PREVIEWSCALE
 PREVIEWWIDTH = WIDTH*PREVIEWSCALE
 
+class pixelCanvas:
+
+   def __init__(self,sarray,scale,offsetx=0,offsety=0):
+      self.sarray = sarray
+      self.scale = scale
+      self.width = len(sarray)
+      self.height = len(sarray[0])
+      self.surface = pygame.Surface((self.width*self.scale,self.height*self.scale))
+      self.offsetx = offsetx
+      self.offsety = offsety
+
+   def get_surface(self):
+      return self.surface
+
+   def get_rect(self):
+      return self.surface.get_rect(top=self.offsety,left=self.offsetx)
+
+   def draw(self):
+
+      for y in range(self.height):
+         for x in range(self.width):
+            color = self.sarray[x,y]
+            self.surface.fill(color, (x*self.scale,y*self.scale,self.scale-1,self.scale-1))
+
+   def checkPoint(self,pos):
+      return self.get_rect().collidepoint(pos)
+
+   def flip(self):
+      self.sarray = self.sarray[::-1,:]
+ 
+   def get_preview(self):
+      return self.sarray.make_surface()
+
+   def coord(self,pos):
+      return ( int((pos[0]-self.offsetx) / self.scale),
+               int((pos[1]-self.offsety) / self.scale) )
+
+   def paint(self,pos,color):
+      (x,y) = self.coord(pos)
+      self.sarray[x,y] = color
+
+   def set_image(self,surface):
+      self.sarray = pygame.PixelArray(surface)
+
+def getFiles(path):
+   files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+   return(files)
 
 def isoPos(pos):
    (x,y) = pos
    ix = x*ISOWIDTH+(y%2)*(ISOWIDTH>>1)
    iy = (y*ISOHEIGHT)>>1
    return(ix,iy)
-
-def scalePos(pos):
-   x = int((pos[0] - OFFSETX)/SCALE)
-   y = int((pos[1] - OFFSETY)/SCALE)
-   return(x,y)
-
-def checkPos(pos):
-   (x,y)=pos
-   if (x>=0) and (x < WIDTH) and (y>=0) and (y < HEIGHT):
-      return True
-   return False
-
-def paint(shapeArray,pos,color):
-   if checkPos(pos):
-      (x,y)=pos
-      shapeArray[x,y] = color
-   return(color,shapeArray.make_surface())
-
-def getFiles(path):
-   files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
-   return(files)
 
 def main():
 
@@ -66,14 +92,11 @@ def main():
 
    shape = pygame.Surface((WIDTH,HEIGHT),SRCALPHA)
    shape.fill(MASKED)
-
    for x in range(10):
       shape.fill(BLACK,(x*2,HEIGHT-11-ISOOFFSET-x,WIDTH-4*x,(x+1)*2+ISOOFFSET))
       shape.fill(WHITE,(x*2,HEIGHT-11-ISOOFFSET-x,WIDTH-4*x,(x+1)*2))
 
-   preview = shape.copy()
-
-   shapeArray = pygame.PixelArray(shape)
+   canvas = pixelCanvas(pygame.PixelArray(shape),SCALE,OFFSETX,OFFSETY)
    (lastX,lastY) = (0,0)
 
    while True:
@@ -88,44 +111,39 @@ def main():
                   color = BLACK
                elif (event.button == 2):
                   color = MASKED
-               (lastColor,preview) = paint(shapeArray,scalePos(event.pos),color)
+               if (canvas.checkPoint(event.pos)):
+                  canvas.paint(event.pos,color)
+                  lastColor = color
             elif event.type == MOUSEMOTION:
-               pos = scalePos(event.pos)
-               if checkPos(pos):
-                  (lastX,lastY) = pos
-               if (event.buttons[0] or event.buttons[1] or event.buttons[2]):
-                  (lastColor,preview) = paint(shapeArray,scalePos(event.pos),lastColor)
+               if (canvas.checkPoint(event.pos) and
+                  (event.buttons[0] or event.buttons[1] or event.buttons[2])):
+                  canvas.paint(event.pos,color)
+                  (lastX,lastY) = canvas.coord(event.pos)
             elif event.type == TEXTINPUT:
                if event.text == 'h':
                   # flip horizontal
-                  shapeArray = shapeArray[::-1,:]
-               elif event.text == 'v':
-                  # flip vertical
-                  shapeArray = shapeArray[:,::-1]
+                  canvas.flip()
+                  #shapeArray = shapeArray[::-1,:]
                elif event.text == 's':
                   # save
                   print("Saved to iso.png")
-                  pygame.image.save(preview,"iso.png")
+                  pygame.image.save(canvas.get_preview(),"iso.png")
                elif event.text == 'l':
                   # load
                   print("Load from iso.png")
-                  shape = pygame.image.load("iso.png")
-                  shapeArray = pygame.PixelArray(shape)
-
-               preview = shapeArray.make_surface();
+                  canvas.set_image(pygame.image.load("iso.png"))
 
       clock.tick(60)
 
       screen.fill(BACKGROUND)
 
+      canvas.draw()
+      screen.blit(canvas.get_surface(),canvas.get_rect())
 
-      for y in range(HEIGHT):
-         for x in range(WIDTH):
-            color = shapeArray[x,y]
-            screen.fill(color, (OFFSETX+x*SCALE,OFFSETY+y*SCALE,SCALE-1,SCALE-1))
-
+      preview = canvas.get_preview()
       previewScaled = pygame.transform.scale(preview,(PREVIEWWIDTH,PREVIEWHEIGHT))
       #previewScaled.fill((200,200,0,128),None,BLEND_RGBA_MULT)
+
       screen.blit(preview,(PREVIEWX,PREVIEWY))
       screen.blit(previewScaled,(PREVIEWX+WIDTH+5,PREVIEWY))
 
