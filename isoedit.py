2bccf0295858dd9a0be867c592ebcb4e6717e4fd
pmwasson <pmwasson@gmail.com>
2021-02-27 17:40:33 -0800

    Started adding gui elements

--- isoedit.py
+++ isoedit.py
@@ -1,6 +1,9 @@
 import os
 import pygame
 from pygame.locals import *
+import pygame_gui
+
+SCREENSIZE = (1024,640)
 
 WHITE  = pygame.Color(255,255,255,255)
 BLACK  = pygame.Color(0,0,0,255)
@@ -14,16 +17,20 @@ ISOHEIGHT = ISOWIDTH>>1
 ISOOFFSET = 4
 
 WIDTH = ISOWIDTH-2
-HEIGHT = 64-8
-OFFSETX = 5
+
+OFFSETX = 600
 OFFSETY = 5
-PREVIEWX = OFFSETX*2 + WIDTH*SCALE
+
+
+HEIGHT = 64-8
+PREVIEWX = 5
 PREVIEWY = OFFSETY
 PREVIEWSCALE = 2
 PREVIEWHEIGHT = HEIGHT*PREVIEWSCALE
 PREVIEWWIDTH = WIDTH*PREVIEWSCALE
 
-class pixelCanvas:
+
+class PixelCanvas:
 
    def __init__(self,sarray,scale,offsetx=0,offsety=0):
       self.sarray = sarray
@@ -67,6 +74,23 @@ class pixelCanvas:
    def set_image(self,surface):
       self.sarray = pygame.PixelArray(surface)
 
+
+class TileList:
+
+   def __init__(self,path):
+      self.path = path
+      self.list = None
+
+   def read(self):
+      self.list = [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path,f)) and (f.find('.png') != -1)] 
+
+   def name_list(self):
+      return([f.split('.')[0] for f in self.list])
+
+   def load_surface(self,name):
+      index = self.name_list().index(name)
+      return pygame.image.load(os.path.join(self.path,self.list[index]))
+
 def getFiles(path):
    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
    return(files)
@@ -80,63 +104,73 @@ def isoPos(pos):
 def main():
 
    # make a list of tile files
-   files = getFiles('tiles')
-   print("files=",files)
-
+   tiles = TileList('tiles')
+   tiles.read()
 
    pygame.init()
-   screen = pygame.display.set_mode((1024, 640),SRCALPHA)
+   screen = pygame.display.set_mode(SCREENSIZE,SRCALPHA)
    pygame.display.set_caption('Arduboy isometric tile editor')
    clock = pygame.time.Clock()
    font = pygame.font.Font(None,32)
 
-   shape = pygame.Surface((WIDTH,HEIGHT),SRCALPHA)
-   shape.fill(MASKED)
-   for x in range(10):
-      shape.fill(BLACK,(x*2,HEIGHT-11-ISOOFFSET-x,WIDTH-4*x,(x+1)*2+ISOOFFSET))
-      shape.fill(WHITE,(x*2,HEIGHT-11-ISOOFFSET-x,WIDTH-4*x,(x+1)*2))
+   manager = pygame_gui.UIManager(SCREENSIZE)
 
-   canvas = pixelCanvas(pygame.PixelArray(shape),SCALE,OFFSETX,OFFSETY)
+   tileSelect = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(options_list=tiles.name_list(),
+                                                                     starting_option=tiles.name_list()[0],
+                                                                     relative_rect=pygame.Rect((0,0),(120,22)),
+                                                                     manager=manager)
+
+
+   shape = tiles.load_surface(tiles.name_list()[0])
+   canvas = PixelCanvas(pygame.PixelArray(shape),SCALE,OFFSETX,OFFSETY)
    (lastX,lastY) = (0,0)
 
    while True:
-      for event in pygame.event.get():
-            if event.type == QUIT:
-               pygame.quit()
-               return
-            elif event.type == MOUSEBUTTONDOWN:
-               if (event.button == 1):
-                  color = WHITE
-               elif (event.button == 3):
-                  color = BLACK
-               elif (event.button == 2):
-                  color = MASKED
-               if (canvas.checkPoint(event.pos)):
-                  canvas.paint(event.pos,color)
-                  lastColor = color
-            elif event.type == MOUSEMOTION:
-               if (canvas.checkPoint(event.pos) and
-                  (event.buttons[0] or event.buttons[1] or event.buttons[2])):
-                  canvas.paint(event.pos,color)
-                  (lastX,lastY) = canvas.coord(event.pos)
-            elif event.type == TEXTINPUT:
-               if event.text == 'h':
-                  # flip horizontal
-                  canvas.flip()
-                  #shapeArray = shapeArray[::-1,:]
-               elif event.text == 's':
-                  # save
-                  print("Saved to iso.png")
-                  pygame.image.save(canvas.get_preview(),"iso.png")
-               elif event.text == 'l':
-                  # load
-                  print("Load from iso.png")
-                  canvas.set_image(pygame.image.load("iso.png"))
 
-      clock.tick(60)
+      time_delta = clock.tick(60)/1000.0
+
+      for event in pygame.event.get():
+         if event.type == QUIT:
+            pygame.quit()
+            return
+         elif event.type == MOUSEBUTTONDOWN:
+            if (event.button == 1):
+               color = WHITE
+            elif (event.button == 3):
+               color = BLACK
+            elif (event.button == 2):
+               color = MASKED
+            if (canvas.checkPoint(event.pos)):
+               canvas.paint(event.pos,color)
+               lastColor = color
+         elif event.type == MOUSEMOTION:
+            if (canvas.checkPoint(event.pos) and
+               (event.buttons[0] or event.buttons[1] or event.buttons[2])):
+               canvas.paint(event.pos,color)
+               (lastX,lastY) = canvas.coord(event.pos)
+         elif event.type == TEXTINPUT:
+            if event.text == 'h':
+               # flip horizontal
+               canvas.flip()
+               #shapeArray = shapeArray[::-1,:]
+            elif event.text == 's':
+               # save
+               print("Saved to iso.png")
+               pygame.image.save(canvas.get_preview(),"iso.png")
+            elif event.text == 'l':
+               # load
+               print("Load from iso.png")
+               canvas.set_image(pygame.image.load("iso.png"))
+         if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+               canvas.set_image(tiles.load_surface(event.text))
+
+         manager.process_events(event)
+
+      manager.update(time_delta)
+
 
       screen.fill(BACKGROUND)
-
       canvas.draw()
       screen.blit(canvas.get_surface(),canvas.get_rect())
 
@@ -153,6 +187,8 @@ def main():
             screen.blit(previewScaled,(PREVIEWX+PREVIEWSCALE*ix,PREVIEWY+PREVIEWHEIGHT+5+PREVIEWSCALE*iy))
 
       screen.blit(font.render("x={:02}, y={:02}".format(lastX-19,HEIGHT-1-lastY-14),True,WHITE,BLACK),(5,600))
+
+      manager.draw_ui(screen)
 
       pygame.display.flip()
 
